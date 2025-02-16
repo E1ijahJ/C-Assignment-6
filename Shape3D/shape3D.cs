37bc9c160b07f25344372d12ce14cf4a18409c65
E1ijahJ <elijahguijoseph@gmail.com>
2025-02-16 15:05:52 -0500

    initial commit

---
+++ Shape3D/shape3D.cs
@@ -0,0 +1,16 @@
+﻿namespace Shape3D;
+
+public abstract class shape3D
+{
+
+public string Name{get; set;} = "3D Shape";
+
+public abstract double GetSurfaceArea();
+
+
+public abstract double GetVolume();
+
+public abstract string Dump();
+}
+
+
