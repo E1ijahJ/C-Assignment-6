6fed9cc0c9db40af1524705e3620a912ef2eab7d
E1ijahJ <elijahguijoseph@gmail.com>
2025-02-18 19:15:59 -0500

    Created Shape Container class and functions for it

---
+++ Shape3D/ShapeContainer.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Shape3D{
+
+public class ShapeContainer{
+private List<shape3D> shapes;
+  public ShapeContainer(){
+  shapes = new List<shape3D>();
+  }
+  public void Create(shape3D shape){
+    shapes.Add(shape);
+  }
+  public shape3D Get(int index){
+    
+  return shapes[index];
+  }
+
+  public void Delete(int index){
+    shapes.RemoveAt(index);
+
+  }
+}
+
+
+}
